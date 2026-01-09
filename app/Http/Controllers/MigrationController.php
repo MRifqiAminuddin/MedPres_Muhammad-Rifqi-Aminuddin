02eab744ca6c79c86de21316e0a98890cc9c7aeb
akunku767 <mrifqi767@gmail.com>
2026-01-09 20:35:44 +0700

    migration

---
+++ app/Http/Controllers/MigrationController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+
+class MigrationController extends Controller
+{
+    public function index(){
+        if (!app()->environment(['local', 'staging'])) {
+            abort(403, 'Forbidden');
+        }
+
+        Artisan::call('migrate', [
+            '--seed'  => true,
+            '--force' => true,
+        ]);
+
+        return response(
+            '<pre>' . Artisan::output() . '</pre>'
+        );
+    }
+
+    public function fresh(){
+        if (!app()->environment(['local', 'staging'])) {
+            abort(403, 'Forbidden');
+        }
+
+        Artisan::call('migrate:fresh', [
+            '--seed'  => true,
+            '--force' => true,
+        ]);
+
+        return response(
+            '<pre>' . Artisan::output() . '</pre>'
+        );
+    }
+
+    public function freshSeed(){
+        if (!app()->environment(['local', 'staging'])) {
+            abort(403, 'Forbidden');
+        }
+
+        Artisan::call('migrate:fresh --seed', [
+            '--seed'  => true,
+            '--force' => true,
+        ]);
+
+        return response(
+            '<pre>' . Artisan::output() . '</pre>'
+        );
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MigrationController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Kelola migration
+Route::prefix('migration')->group(function () {
+    Route::get('/', [MigrationController::class, 'index'])->name('migration.index');
+    Route::get('/fresh', [MigrationController::class, 'fresh'])->name('migration.fresh');
+    Route::get('/fresh-seed', [MigrationController::class, 'freshSeed'])->name('migration.freshSeed');
 });
